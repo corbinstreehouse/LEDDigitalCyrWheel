fe5def597d77f2323cfbc0d1fc0e4d33a380cdfb
corbin dunn <corbin@corbinstreehouse.com>
2014-01-17 10:58:12 -0800

    delete stuff in builds.

--- LEDDigitalCyrWheel/Builds/eeprom.d
+++
@@ -1,3 +0,0 @@
-Builds/eeprom.o: \
- /Applications/Arduino.app/Contents/Resources/Java/hardware/teensy/cores/teensy3/eeprom.c \
- /Applications/Arduino.app/Contents/Resources/Java/hardware/teensy/cores/teensy3/mk20dx128.h
--- LEDDigitalCyrWheel/Builds/yield.d
+++
@@ -1,2 +0,0 @@
-Builds/yield.o: \
- /Applications/Arduino.app/Contents/Resources/Java/hardware/teensy/cores/teensy3/yield.c
