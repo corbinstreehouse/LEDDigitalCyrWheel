bdd4c5c2f5e0029dd2d330a049a72e7001f490f9
corbin dunn <corbin@corbinstreehouse.com>
2016-04-07 20:16:18 -0700

    6mhz?

--- LEDDigitalCyrWheel/CyrWheelLEDPatterns.cpp
+++ LEDDigitalCyrWheel/CyrWheelLEDPatterns.cpp
@@ -128,7 +128,8 @@ protected:
 // 12Mhz
 // Try 24? -- see issues..
 // 6 Mhz!! geez..so slow.. 8 mhz might work
-static SPISettings m_spiSettings = SPISettings(8000000, MSBFIRST, SPI_MODE0);
+// back to 6 Mhz...
+static SPISettings m_spiSettings = SPISettings(6000000, MSBFIRST, SPI_MODE0);
 
 void CyrWheelLEDPatterns::_spiBegin() {
     // DOUT/ MOSI on pin 7 for this
@@ -139,9 +140,8 @@ void CyrWheelLEDPatterns::_spiBegin() {
     SPI.begin(); // resets the pins
     
     // corbin?? enable DSE??
-
-    CORE_PIN14_CONFIG = PORT_PCR_DSE | PORT_PCR_MUX(2); // SCK0 = 13 (PTC5)
-    CORE_PIN7_CONFIG = PORT_PCR_DSE | PORT_PCR_MUX(2); // DOUT/MOSI = 7 (PTD2)
+//    CORE_PIN14_CONFIG = PORT_PCR_DSE | PORT_PCR_MUX(2); // SCK0 = 13 (PTC5)
+//    CORE_PIN7_CONFIG = PORT_PCR_DSE | PORT_PCR_MUX(2); // DOUT/MOSI = 7 (PTD2)
 
     SPI.beginTransaction(m_spiSettings);
 }
